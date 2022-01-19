03885447b9126ac625b10c2733e1f6d76eb0a22e
Kennion Gubler <gublerkennion@gmail.com>
2022-01-18 20:50:35 -0700

    first commit

---
+++ Swift Language/hello-world.swift
@@ -0,0 +1,32 @@
+// print("Hello, World!") 
+
+// for name in names{
+//     print("Hello, World!") 
+// }
+let names = ["Brian", "Bailey", "George", "Henry"]
+//This is a lovely example of iterating through a list in Swift
+for name in names where name != "George"{
+  print(name) 
+}
+//This is our classic range for loop
+for i in 1...10 {
+    print(i)
+}
+// The stride keyword allows us to set an increment
+for i in stride(from: 1, to: 10, by: 5) {
+    print(i)
+}
+
+let dictionary = [1: "Hello",
+2: "Goodbye", 3: "Test"]
+
+print(dictionary[2])
+
+let x = 1;
+
+print(x);
+
+public class Player
+{
+  
+}
